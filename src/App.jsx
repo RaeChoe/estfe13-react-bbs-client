dc3417c7ab7aa89dd7e140d69503d4abaadafe87
RaeChoe <picasomati@gmail.com>
2026-07-03 15:10:01 +0900

    Implement routing with react-router; add View component for post details; enhance Write component for editing posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { Route, Routes } from "react-router";
 import BoardList from "./components/BoardList";
 import Write from "./components/Write";
+import View from "./components/View";
+import { useState } from "react";
 
 function App() {
+  const [boardId, setBoardId] = useState(0);
+  const [isModifyMode, setIsModifyMode] = useState(false);
+
+  const handleModify = _id => {
+    setBoardId(_id);
+    setIsModifyMode(true);
+  };
+
+  const handleCancel = () => {
+    setBoardId(0);
+    setIsModifyMode(false);
+  };
+
   return (
     <div className="container">
       <h1>React BBS</h1>
-      <BoardList />
-      <hr />
-      <Write />
+      <Routes>
+        <Route path="/" element={<BoardList />} />
+        <Route
+          path="/write"
+          element={
+            <Write isModifyMode={isModifyMode} boardId={boardId} handleCancel={handleCancel} />
+          }
+        />
+        <Route path="/view/:id" element={<View handleModify={handleModify} />} />
+      </Routes>
     </div>
   );
 }
