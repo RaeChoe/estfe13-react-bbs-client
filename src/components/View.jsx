ad54a88026a258d54a1d353d99f397b5ff2d8cc6
RaeChoe <picasomati@gmail.com>
2026-07-03 15:37:19 +0900

    feat: 삭제 후 홈으로 이동

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,7 +1,7 @@
 import Button from "react-bootstrap/Button";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router";
+import { Link, useParams, useNavigate } from "react-router";
 
 export default function View({ handleModify }) {
   const [content, setContent] = useState({
@@ -12,6 +12,8 @@ export default function View({ handleModify }) {
   const [isError, setIsError] = useState(false);
 
   const { id } = useParams();
+  let navigate = useNavigate();
+
   useEffect(() => {
     axios
       .get(`http://localhost:3000/view?id=${id}`)
@@ -56,6 +58,22 @@ export default function View({ handleModify }) {
     handleModify(id);
   };
 
+  const handleDelete = () => {
+    if (window.confirm("정말 삭제할까요?")) {
+      axios
+        .post("http://localhost:3000/delete", {
+          id: id,
+        })
+        .then(() => {
+          navigate("/");
+        })
+        .catch(error => {
+          console.error(error);
+        })
+        .finally(() => {});
+    }
+  };
+
   return (
     <>
       <h2>글 제목</h2>
@@ -73,7 +91,9 @@ export default function View({ handleModify }) {
         <Button variant="secondary" onClick={handleClick}>
           수정
         </Button>
-        <Button variant="danger">삭제</Button>
+        <Button variant="danger" onClick={handleDelete}>
+          삭제
+        </Button>
       </div>
     </>
   );
